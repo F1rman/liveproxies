6b53a34354623503ea32c96f820edcd837d58f87
F1rman <ihorkharchyshyn@gmail.com>
2023-02-19 23:08:00 +0200

    faq

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -2,143 +2,122 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Questions() {
-    const faq =  [
-      {
-        title:'',
-        name:'',
-      }
-    ];
-    const [open,setOpen] = useState(0);
-    function collapse(e,i){
-        console.log(e,i)
-        e.preventDefault()
-        if (open==i){
-            setOpen(0)
-        }
-        else{
-            setOpen(i)
-        }
-    }
-  
+  const faq = [
+    {
+      title: "Can I purchase and use different proxy plans on a single account?",
+      description:
+        "Yes, it is possible to utilize multiple, different proxy plans on a single account.",
+    },
+    {
+      title: "Can all available geolocations be used in the same plan?",
+      description:
+        "No, each plan can only be used for one of the pre-selected geolocations: US, CA, UK, and RL (mixed pool, excluding US, UK, and CA).",
+    },
+    {
+      title: "How can I get proxies specific to a certain state or city?",
+      description:
+        "We only offer state-specific proxies for the United States. These proxies are rotating-residential and are available on a per-state basis per plan.",
+    },
+    {
+      title: "Do you provide ISP/DC proxies?",
+      description:
+        "No, we do not currently support ISP/DC proxies.",
+    },
+    {
+      title: "What payment methods are supported?",
+      description:
+        "We work with Stripe and accept several different payment forms, such as Google Pay, Apple Pay, AMEX, VISA, and Mastercard. PayPal, Zelle, and wire transfers are available upon request.",
+    },
+    {
+      title: "Why is having a Discord account and joining the Live Discord server essential?",
+      description:
+        "By joining our private Discord server, which already has 14K+ members, you can reach out quickly to resolve any issues, receive instant notifications, real-time support, and many more.",
+    },
+    {
+      title: "Can I resell Live Proxies?",
+      description:
+        "No, reselling is not allowed and we do not provide a reseller API.",
+    },
+    {
+      title: "Do you have an affiliate program?",
+      description:
+        "No, we do not have an affiliate program at this time.",
+    },
+  ];
+  const [open, setOpen] = useState(-1);
+
+
   return (
     <section>
-   <div className="live_proxies_faqs_main_con">     
-     <div className="container live_proxies_faqs_con">
-      <h2>Frequently asked <span>questions</span></h2>
-      <h6>Please visit our <Link href="https://helpcenter.liveproxies.io/hc/en-us" target="_blank">Help Center</Link> if you have questions that are not listed below</h6>
-       <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-         <div className="panel panel-default">
-           <div className={open == 1 ? "panel-heading active":"panel-heading "}  role="tab" id="headingTwo">
-             <h4 className="panel-title">
-               <a className="collapsed" onClick={(e)=>{
-                collapse(e,1)
-               }} role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">What are the PRIVATE Live Proxies?
-               </a>
-             </h4>
-           </div>
-           <div id="collapseTwo" className={open == 1 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingTwo">
-             <div className="panel-body">Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</div>
-           </div>
-         </div> 
-           <div className="panel panel-default">
-             <div className={open == 2 ? "panel-heading active":"panel-heading "} role="tab" id="headingFour">
-               <h4 className="panel-title">
-                 <a className="collapsed" onClick={(e)=>{
-                collapse(e,2)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseThree">How can I join the Live Proxies Discord server?
-                 </a>
-               </h4>
-             </div>
-             <div id="collapseFour" className={open == 2 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingFour">
-		        <div className="panel-body"><span>Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</span>
+      <div className="live_proxies_faqs_main_con">
+        <div className="container live_proxies_faqs_con">
+          <h2>
+            Frequently asked <span>questions</span>
+          </h2>
+          <h6>
+            Please visit our{" "}
+            <Link
+              href="https://helpcenter.liveproxies.io/hc/en-us"
+              target="_blank"
+            >
+              Help Center
+            </Link>{" "}
+            if you have questions that are not listed below
+          </h6>
+          <div
+            className="panel-group"
+            id="accordion"
+            role="tablist"
+            aria-multiselectable="true"
+          >
+            {faq.map((item,i) => {
+              return (
+                <div key={item.title} className="panel panel-default">
+                  <div
+                    className={
+                      open == i ? "panel-heading active" : "panel-heading "
+                    }
+                    role="tab"
+                    id="headingTwo"
+                  >
+                    <h4 className="panel-title">
+                      <a
+                        className="collapsed"
+                        onClick={(e) => {
+                          if (i == open){
+                          setOpen(-1);
+                          }
+                          else{
+                            setOpen(i);
+
+                          }
+                        }}
+                        role="button"
+                        data-toggle="collapse"
+                        data-parent="#accordion"
+                        aria-expanded="false"
+                        aria-controls="collapseTwo"
+                      >
+                        {item.title}
+                      </a>
+                    </h4>
+                  </div>
+                  <div
+                    id="collapseTwo"
+                    className={
+                      open == i ? "panel-collapse " : "panel-collapse collapse"
+                    }
+                    role="tabpanel"
+                    aria-labelledby="headingTwo"
+                  >
+                    <div className="panel-body">{item.description}</div>
+                  </div>
                 </div>
-             </div>
-           </div>
-           <div className="panel panel-default">
-             <div className={open == 3 ? "panel-heading active":"panel-heading "} role="tab" id="headingFive">
-               <h4 className="panel-title">
-                 <a className="collapsed"  onClick={(e)=>{
-                collapse(e,3)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseThree">Do you guys offer no data-limited plans?</a>
-               </h4>
-             </div>
-             <div id="collapseFive" className={open == 3 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingFive">
-              <div className="panel-body">Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</div>
-             </div>
+              );
+            })}
           </div>
-         <div className="panel panel-default">
-           <div className={open == 4 ? "panel-heading active":"panel-heading "} role="tab" id="headingThree">
-             <h4 className="panel-title">
-               <a className="collapsed"  onClick={(e)=>{
-                collapse(e,4)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">Do you ever plan on making ISP or DC proxies?
-               </a>
-             </h4>
-           </div>
-           <div id="collapseSeven" className={open == 4 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingThree">
-             <div className="panel-body">Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</div>
-             </div>
-           </div>
-           <div className="panel panel-default">
-             <div className={open == 5 ? "panel-heading active":"panel-heading "} role="tab" id="headingEight">
-               <h4 className="panel-title">
-                 <a className="collapsed"  onClick={(e)=>{
-                collapse(e,5)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseThree">Are those residential proxies only?
-                 </a>
-               </h4>
-             </div>
-             <div id="collapseEight" className={open == 5 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingEight">
-		         <div className="panel-body"><span>Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</span>
-                 </div>
-             </div>
-           </div>
-           <div className="panel panel-default">
-             <div className={open == 6 ? "panel-heading active":"panel-heading "} role="tab" id="headingNine">
-               <h4 className="panel-title">
-                 <a className="collapsed"  onClick={(e)=>{
-                collapse(e,6)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseNine" aria-expanded="false" aria-controls="collapseThree">Can I filter the proxies by state, city, and ISP carrier?</a>
-               </h4>
-             </div>
-             <div id="collapseNine" className={open == 6 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingNine">
-		         <div className="panel-body">Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</div>
-             </div>
-           </div>
-           <div className="panel panel-default">
-             <div className={open == 7 ? "panel-heading active":"panel-heading "} role="tab" id="headingTen">
-               <h4 className="panel-title">
-                 <a className="collapsed"  onClick={(e)=>{
-                collapse(e,7)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTen" aria-expanded="false" aria-controls="collapseThree">What are the best use cases for Live Proxies?</a>
-               </h4>
-             </div>
-             <div id="collapseTen" className={open == 7 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingTen">
-                 <div className="panel-body">Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</div>
-             </div>
-           </div>
-           <div className="panel panel-default">
-             <div className={open == 8 ? "panel-heading active":"panel-heading "} role="tab" id="headingEleven">
-               <h4 className="panel-title">
-                 <a className="collapsed"  onClick={(e)=>{
-                collapse(e,8)
-               
-               }}  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven" aria-expanded="false" aria-controls="collapseThree">What are the websites Live Proxies works best?</a>
-               </h4>
-             </div>
-             <div id="collapseEleven" className={open == 8 ? "panel-collapse ":"panel-collapse collapse"} role="tabpanel" aria-labelledby="headingEleven">
-              <div className="panel-body">Technically it&apos;s possible, but you won&apos;t find this option on your dashboard. To match your custom solution requirements, you must open a ticket on our Discord server or email us.</div>
-             </div>
-           </div>
         </div>
       </div>
-	</div>
- </section>  
+    </section>
   );
 }
--- src/modules/dashboard/dashboard_questions.jsx
+++ src/modules/dashboard/dashboard_questions.jsx
@@ -2,231 +2,88 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function DashboardQuestions(props) {
-  const [open, setOpen] = useState(0);
-  function collapse(e, i) {
-    console.log(e, i);
-    e.preventDefault();
-    if (open == i) {
-      setOpen(0);
-    } else {
-      setOpen(i);
-    }
-  }
+  const faq = [
+    {
+      title: "Can I purchase and use different proxy plans on a single account?",
+      description:
+        "Yes, it is possible to utilize multiple, different proxy plans on a single account.",
+    },
+    {
+      title: "Can all available geolocations be used in the same plan?",
+      description:
+        "No, each plan can only be used for one of the pre-selected geolocations: US, CA, UK, and RL (mixed pool, excluding US, UK, and CA).",
+    },
+    {
+      title: "How can I get proxies specific to a certain state or city?",
+      description:
+        "We only offer state-specific proxies for the United States. These proxies are rotating-residential and are available on a per-state basis per plan.",
+    },
+    {
+      title: "Do you provide ISP/DC proxies?",
+      description:
+        "No, we do not currently support ISP/DC proxies.",
+    },
+    {
+      title: "What payment methods are supported?",
+      description:
+        "We work with Stripe and accept several different payment forms, such as Google Pay, Apple Pay, AMEX, VISA, and Mastercard. PayPal, Zelle, and wire transfers are available upon request.",
+    },
+    {
+      title: "Why is having a Discord account and joining the Live Discord server essential?",
+      description:
+        "By joining our private Discord server, which already has 14K+ members, you can reach out quickly to resolve any issues, receive instant notifications, real-time support, and many more.",
+    },
+    {
+      title: "Can I resell Live Proxies?",
+      description:
+        "No, reselling is not allowed and we do not provide a reseller API.",
+    },
+    {
+      title: "Do you have an affiliate program?",
+      description:
+        "No, we do not have an affiliate program at this time.",
+    },
+  ];
+  const [open, setOpen] = useState(-1);
+
   return (
     <div className="overview_live_proxies_faqs_main_con">
       <div className="  overview_live_proxies_faqs_con">
         <h2>Frequently asked questions</h2>
-        <div
-          onClick={(e) => {
-            collapse(e, 1);
-          }}
-          className={
-            open == 1
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>What are the PRIVATE Live Proxies?</p>
-          </div>
-          <div
-            style={{ display: open == 1 ? "block" : "none" }}
-            className={
-              open == 1
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        <div
-          onClick={(e) => {
-            collapse(e, 2);
-          }}
-          className={
-            open == 2
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>How can I join the Live Proxies Discord server?</p>
-          </div>
-          <div
-            style={{ display: open == 2 ? "block" : "none" }}
-            className={
-              open == 2
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        <div
-          onClick={(e) => {
-            collapse(e, 3);
-          }}
-          className={
-            open == 3
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>Do you guys offer no data-limited plans?</p>
-          </div>
-          <div
-            style={{ display: open == 3 ? "block" : "none" }}
-            className={
-              open == 3
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        <div
-          onClick={(e) => {
-            collapse(e, 4);
-          }}
-          className={
-            open == 4
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>Do you ever plan on making ISP or DC proxies?</p>
-          </div>
-          <div
-            style={{ display: open == 4 ? "block" : "none" }}
-            className={
-              open == 4
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        <div
-          onClick={(e) => {
-            collapse(e, 5);
-          }}
-          className={
-            open == 5
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>Are those residential proxies only?</p>
-          </div>
-          <div
-            style={{ display: open == 5 ? "block" : "none" }}
-            className={
-              open == 5
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        
-        <div
-          onClick={(e) => {
-            collapse(e, 6);
-          }}
-          className={
-            open == 6
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>Can I filter the proxies by state, city, and ISP carrier?</p>
-          </div>
-          <div
-            style={{ display: open == 6 ? "block" : "none" }}
-            className={
-              open == 6
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
+        {faq.map((item, i) => {
+          return (
+            <div
+              key={item.title}
+              onClick={(e) => {
+                if (i == open) {
+                  setOpen(-1);
+                } else {
+                  setOpen(i);
+                }
+              }}
+              className={
+                open == i
+                  ? "overview_live_proxies_faqs_module active"
+                  : "overview_live_proxies_faqs_module "
+              }
+            >
+              <div className={"overview_live_proxies_faqs_head"}>
+                <p>{item.title}</p>
+              </div>
+              <div
+                style={{ display: open == i ? "block" : "none" }}
+                className={
+                  open == i
+                    ? "overview_live_proxies_faqs_body active"
+                    : "overview_live_proxies_faqs_body"
+                }
+              >
+              {item.description}
+              </div>
+            </div>
+          );
+        })}
 
-        <div
-          onClick={(e) => {
-            collapse(e, 7);
-          }}
-          className={
-            open == 7
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>What are the best use cases for Live Proxies?</p>
-          </div>
-          <div
-            style={{ display: open == 7 ? "block" : "none" }}
-            className={
-              open == 7
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        <div
-          onClick={(e) => {
-            collapse(e, 8);
-          }}
-          className={
-            open == 8
-              ? "overview_live_proxies_faqs_module active"
-              : "overview_live_proxies_faqs_module "
-          }
-        >
-          <div className={"overview_live_proxies_faqs_head"}>
-            <p>What are the websites Live Proxies works best?</p>
-          </div>
-          <div
-            style={{ display: open == 8 ? "block" : "none" }}
-            className={
-              open == 8
-                ? "overview_live_proxies_faqs_body active"
-                : "overview_live_proxies_faqs_body"
-            }
-          >
-            Technically it&apos;s possible, but you won&apos;t find this option
-            on your dashboard. To match your custom solution requirements, you
-            must open a ticket on our Discord server or email us.
-          </div>
-        </div>
-        
       </div>
     </div>
   );
